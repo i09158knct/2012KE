c47f238063af3129dc7e29ddcf0e12380dabce27
i09158knct <i09158knct@gmail.com>
2012-10-19 14:26:18 +0900

    fix format

--- ke18/javascript/neural_networks.js
+++ ke18/javascript/neural_networks.js
@@ -1,38 +1,31 @@
 (function(){
   var root = this;
-  var ke17 = {};
+  var neural_networks = {};
 
   // via Underscore
   if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
-      exports = module.exports = ke17;
+      exports = module.exports = neural_networks;
     }
-    exports.ke17 = ke17;
+    exports.neural_networks = neural_networks;
   } else {
-    root.ke17 = ke17;
+    root.neural_networks = neural_networks;
   }
 
-  ke17.genSigmoid = function(gain){
-    if(gain === undefined) gain = 1;
-    return function(x, offset){
-      if(offset === undefined) offset = 1;
-      return 1 / (1 + Math.exp(offset - gain * x));
-    };
-  };
-
-  ke17.Node = function(options){
+  neural_networks.Node = function(options) {
     options = options || {};
     this.inputWeights = options.inputWeights || [1, 1];
     this.threshold = options.threshold || 1;
-    this.evaluationFunction =
-      options.evaluationFunction ||
-      function(sum, threshold){return sum >= threshold ? 1 : 0;};
-    this.input = function(inputs){
-      var i, sum = 0;
-      for(i in inputs){
+    this.evaluate =
+      options.evaluate ||
+      function(sum, threshold) { return sum >= threshold ? 1 : 0; };
+    this.input = function(inputs) {
+      var sum = 0, output, i;
+      for(i in inputs) {
         sum += inputs[i] * (this.inputWeights[i] || 0);
       }
-      return this.evaluationFunction(sum, this.threshold);
+      output = this.evaluate(sum, this.threshold);
+      return output;
     };
   };
 }).call(this);
