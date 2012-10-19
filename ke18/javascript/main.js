27ad678e63794157e55741fc904c96dd7ac0dd3d
i09158knct <i09158knct@gmail.com>
2012-10-19 16:26:46 +0900

    fix variable name

--- ke18/javascript/main.js
+++ ke18/javascript/main.js
@@ -24,8 +24,8 @@ var sys = require('sys');
 
   var stepCount = 0;
   var feedback = function(inputs, output, node) {
-    var correctOutputs = supervisorySignalTable[inputs][0];
-    var deltaOutput = correctOutputs - output;
+    var correctOutput = supervisorySignalTable[inputs][0];
+    var deltaOutput = correctOutput - output;
     var deltaInputWeights = inputs.map(function(input, index) {
       return learningRate * deltaOutput * input;
     });
@@ -38,7 +38,7 @@ var sys = require('sys');
     console.log(
       ++stepCount,
       inputs.toString(),
-      correctOutputs,
+      correctOutput,
       node.inputWeights.toString(),
       node.threshold,
       output,
@@ -59,9 +59,9 @@ var sys = require('sys');
   // to don't make functions within a loop (and to silence jsLint
   var comparison = function (pairIO, index) {
     var inputs = pairIO[0];
-    var correctOutputs = pairIO[1][0];
+    var correctOutput = pairIO[1][0];
     var output = node.input(inputs);
-    var output_is_correct = (output === correctOutputs);
+    var output_is_correct = (output === correctOutput);
     outputs_is_all_correct &= output_is_correct;
   };
 
