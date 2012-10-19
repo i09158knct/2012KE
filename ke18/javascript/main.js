579687226ccbbae2c86df94bc2ae98a79162cf36
i09158knct <i09158knct@gmail.com>
2012-10-19 16:15:26 +0900

    add main.js

--- ke18/javascript/main.js
+++ ke18/javascript/main.js
@@ -1,25 +1,73 @@
-var nn = nn || require('./neural_networks.js');
+var neural_network = neural_network || require('./neural_network.js');
+var sys = require('sys');
+
 (function(){
-
-  var node = new nn.Node({
-    inputWeights: [3, 2, -1],
-    threshold: 1
-  });
-
-  var n = 1000000;
-  var acc;
-  var checks = [
-    [1, 1, 0],
-    [1, -1, 0],
-    [0, 1, 0]
+  var supervisorySignalData = [
+    [[0, 0], [0]],
+    [[0, 1], [1]],
+    [[1, 0], [1]],
+    [[1, 1], [1]]
   ];
 
-  console.log("--- # javascript");
-  var check_number, i;
-  for(check_number in checks){
-    for(i=0,acc=0; i<n; i++){
-      acc += node.input(checks[check_number]);
-    }
-    console.log("-", acc/n);
+  function parseSupervisorySignalData(ssData) {
+    var ssTable = {};
+    ssData.forEach(function(pairIO, index) {
+      var inputs = pairIO[0];
+      var correctOutput = pairIO[1][0];
+      ssTable[inputs] = correctOutput;
+    });
+    return ssTable;
   }
+
+  var supervisorySignalTable = parseSupervisorySignalData(supervisorySignalData);
+  var learningRate = 1;
+
+  var stepCount = 0;
+  var feedback = function(inputs, output, node) {
+    var correctOutput = supervisorySignalTable[inputs];
+    var deltaOutput = correctOutput - output;
+    var deltaInputWeights = inputs.map(function(input, index) {
+      return learningRate * deltaOutput * input;
+    });
+    var deltaThreshold = - learningRate * deltaOutput;
+    node.inputWeights = node.inputWeights.map(function(inputWeight, index) {
+      return inputWeight + deltaInputWeights[index];
+    });
+    node.threshold = node.threshold + deltaThreshold;
+
+    console.log(
+      ++stepCount,
+      inputs.toString(),
+      correctOutput,
+      node.inputWeights.toString(),
+      node.threshold,
+      output,
+      deltaOutput,
+      deltaInputWeights.toString(),
+      deltaThreshold
+    );
+  };
+
+  var node = new neural_network.Node({
+    inputWeights: [0, 0],
+    threshold: 0,
+    onOutput: feedback
+  });
+
+  var outputs_is_all_correct = false;
+
+  // to don't make functions within a loop (and to silence jsLint
+  var comparison = function (pairIO, index) {
+    var inputs = pairIO[0];
+    var correctOutput = pairIO[1][0];
+    var output = node.input(inputs);
+    var output_is_correct = (output === correctOutput);
+    outputs_is_all_correct &= output_is_correct;
+  };
+
+  while(!outputs_is_all_correct) {
+    outputs_is_all_correct = true;
+    supervisorySignalData.forEach(comparison);
+  }
+
 }).call(this);
