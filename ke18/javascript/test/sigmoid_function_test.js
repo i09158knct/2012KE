3bba3b621c998d8b9d1aa38683272b6af2586b05
i09158knct <i09158knct@gmail.com>
2012-10-20 15:25:01 +0900

    remove test file for sigmoid function

--- ke18/javascript/test/sigmoid_function_test.js
+++
@@ -1,9 +0,0 @@
-TestCase("sigmoidFunctionTest", {
-  setUp: function(){},
-  tearDown: function(){},
-
-  "test genSigmoid() should return standard sigmoid function": function(){
-    var sigmoid = ke17.genSigmoid();
-    assertEquals(sigmoid(1), 0.5);
-  }
-});
